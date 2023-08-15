ab0a886216328b4d61f9ac75427f6a52b1d00293
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-08-15 21:38:49 +0300

    Add HTML template

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   plugins: [
-    new HtmlWebpackPlugin({title: 'Output Management',}),
+    new HtmlWebpackPlugin({ template: './src/index.html'}),
   ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
